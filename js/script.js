c10b089cf5a781702b0e402d8fa9e8466c914091
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2021-06-23 13:05:41 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,53 @@
+var Animacao = {
+    corHexa : ["#FF0000", "#FFFF00", "#00FF00", "#0000FF", "#FF1493", "#FF4500", "#4B0082"],
+    corName : ["Vermelho", "Amarelo", "Verde", "Azul", "Rosa", "Laranja", "Roxo"],
+    botaoDesabilitado: "Aguarde",
+    botaoHabilitado:"Iniciar Animação",
+    boxCor: "",
+    botao : "",
+    timer : "",
+    indexCorAtual :0,
+    alternarCorBox:() => {
+        Animacao.botao.innerText = Animacao.botaoDesabilitado
+
+        Animacao.time = setInterval(() => {
+
+            if(Animacao.indexCorAtual > Animacao.corHexa.length - 1) {
+                clearInterval(Animacao.time)
+                Animacao.indexCorAtual = 0
+                Animacao.botao.innerText = Animacao.botaoHabilitado
+                Animacao.botao.disabled = false
+                document.querySelector('.container').prepend(Animacao.boxCor)
+                return;
+            }
+            
+            Animacao.botao.disabled = true
+            Animacao.boxCor.style.transform = "scale(1)"
+
+            Animacao.boxCor.style.backgroundColor = Animacao.corHexa[Animacao.indexCorAtual]
+            Animacao.boxCor.innerText = Animacao.corName[Animacao.indexCorAtual]
+
+            Animacao.indexCorAtual += 1;
+
+           
+        }, 1000)
+    },
+    iniciarAnimacao: () => {
+        Animacao.botao = document.querySelector(".botao")
+        Animacao.botao.innerText = Animacao.botaoHabilitado
+
+        Animacao.boxCor = document.querySelector(".box-cor")
+
+        Animacao.botao.addEventListener("click", () => {
+            Animacao.alternarCorBox();
+        })
+    },
+    init: () => {
+        Animacao.iniciarAnimacao()
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    Animacao.init()
+})
